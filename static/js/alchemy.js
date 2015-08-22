b0c219c44f04551b80d6ca7a2a362666305a6ea2
Zhila <zfemadi@gmail.com>
2015-08-22 10:10:27 -0700

    final version

--- static/js/alchemy.js
+++ static/js/alchemy.js
@@ -14,43 +14,51 @@ var AlchemyNewsNetwork = function() {
 	// See my note above about exposing API keys and other sensitive information.
 	this.ALCHEMY_API_KEY = '8cff27cd2fc82e9840478030cae451ea59cf4816';
 	this.BASE_MEDIA_QUERY_START = ['https://access.alchemyapi.com/calls/data/GetNews?apikey=', this.ALCHEMY_API_KEY, '&return=enriched.url.title,enriched.url.url,enriched.url.author,enriched.url.publicationDate,enriched.url.'];
-	this.BASE_MEDIA_QUERY_END ='&q.enriched.url.entities.entity=|text=AlchemyApi,type=company|&count=20&outputMode=json';
-	this.BASE_STACKOVERFLOW ='https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&tagged=alchemyapi&';
+	this.BASE_MEDIA_QUERY_END ='|&limit=10&outputMode=json';
+	this.BASE_STACKOVERFLOW ='https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&';
 	var isOpen = false;
 	var panelAccordion = document.getElementById('panel-accordion');
 	var originalAccordion = document.getElementById('accordion-reset').cloneNode(true);
+
 	// Use one main event listener for the body. 
 	document.body.addEventListener("click", function(e) {
 		var target = e.target;
 		if (target.classList.contains('submit')) {
-			panelAccordion.innerHTML = originalAccordion.innerHTML;
+			var form = document.getElementsByTagName('form')[0];
+			if (form.checkValidity()) {
+				panelAccordion.innerHTML = originalAccordion.innerHTML;
 
-			this.initQueryProcess(
-				document.getElementById('noun-select').value,
-				document.getElementById('verb-select').value,
-				document.getElementById('time-select').value
-			);
+				this.initQueryProcess(
+					document.getElementById('noun-select').value,
+					document.getElementById('verb-select').value,
+					document.getElementById('subject-input').value,
+					document.getElementById('time-select').value
+					);
 
-			if (!isOpen) {
-				panelAccordion.classList.remove('hidden');
-				isOpen = true;
-			} 
+				if (!isOpen) {
+					panelAccordion.classList.remove('hidden');
+					isOpen = true;
+				}
+			} else {
+				form.classList.add('invalid');
+			}
 		}
 	}.bind(this));	
 };
 
-// Starts the process for running an API call.
-AlchemyNewsNetwork.prototype.initQueryProcess = function (noun, verb, time) {
+// Start the process for running an API call.
+AlchemyNewsNetwork.prototype.initQueryProcess = function (noun, verb, subject, time) {
 	// First, get common components to all queries. 
 	var times = this.buildCommonQueryComponents(time);
 	var alchemyQueryString;
 	// Then, build the custom query string. Selecting 'new users' and 'power users' first makes a call to StackExchange, then feeds that info into AlchemyAPI.
 	if (noun == 'media') {
-		this.generateMediaQueryString(verb, times);
+		this.generateMediaQueryString(subject, verb, times);
 	} else {
-		this.generateStackOverflowQueryString(verb, times);
+		this.generateStackOverflowQueryString(subject, verb, times);
 	}
-}
+};
+
 
 // Build common components used by all query strings.
 // Returns a dict with start and end times, in epoch time.
@@ -63,27 +71,33 @@ AlchemyNewsNetwork.prototype.buildCommonQueryComponents = function(time) {
 	return {'start_time': start.toString(), 'end_time': end.toString()}
 };
 
+
 // Generates a query string for the query about the media/bloggers, which uses the Alchemy News Api.
 // Returns values to be passed into Handlebars.js.
-AlchemyNewsNetwork.prototype.generateMediaQueryString = function(verb, times) {
+AlchemyNewsNetwork.prototype.generateMediaQueryString = function(subject, verb, times) {
 	var queryString;
 	var searchType = (verb == 'keywords') ? 'concepts' : 'docSentiment';
-	var middleOfQuery = [searchType, '&start=', times.start_time, '&end=',  times.end_time].join('');
+	var middleOfQuery = [searchType, '&start=', times.start_time, '&end=',  times.end_time, '&q.enriched.url.entities.entity=|text=' + subject].join('');
 
 	queryString = this.BASE_MEDIA_QUERY_START.join('') + middleOfQuery + this.BASE_MEDIA_QUERY_END;
 	this.runQuery(queryString, function(data){this.prettyPrintJson(data)}.bind(this));
 };
 
+
 // Generates a query string for the query about users, which users StackOverflow.
 // Returns values to be passed into Handlebars.js.
-AlchemyNewsNetwork.prototype.generateStackOverflowQueryString = function(verb, times) {
-	var queryString = [this.BASE_STACKOVERFLOW, 'fromdate=', times.start_time, '&todate=', times.end_time].join('');
+AlchemyNewsNetwork.prototype.generateStackOverflowQueryString = function(subject, verb, times) {
+	var queryString = [this.BASE_STACKOVERFLOW, 'tagged=', subject, '&fromdate=', times.start_time, '&todate=', times.end_time].join('');
 	return this.runQuery(
 		queryString, function(data){this.stackOverflowToAlchemy(verb, data)}.bind(this));
 };
 
+
 // Converts StackOverflow data to a format that can be easily read by AlchemyAPI.
+// If there's data from StackOverflow, it returns the results from sending the converted text to Alchemy's language API.
 AlchemyNewsNetwork.prototype.stackOverflowToAlchemy = function(verb, data) {
+	console.log('StackOverflow results to send to AlchemyApi:');
+	console.log(data);
 	if (data['items'].length > 0) {
 		// prep data here
 		var textData = [];
@@ -107,36 +121,36 @@ AlchemyNewsNetwork.prototype.stackOverflowToAlchemy = function(verb, data) {
 		var query = ['http://access.alchemyapi.com/calls/text/', searchType, '?apikey=', this.ALCHEMY_API_KEY, '&outputMode=json&text=', encodeURIComponent(textData.join(' '))];
 		return this.runQuery(query.join(''), callback);
 	} else {
-		this.noResults();
+		noResults();
 	}
 };
 
 // Run the query and pass in a callback, to be used when data is returned.
+// Returns JSON response text.
 AlchemyNewsNetwork.prototype.runQuery = function(url, callback) {
-	console.log(url);
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, true);
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", url, true);
 
-    xmlHttp.onload = function (e) {
-      if (xmlHttp.readyState === 4) {
-        if (xmlHttp.status === 200) {
+	xmlHttp.onload = function (e) {
+		if (xmlHttp.readyState === 4) {
+			if (xmlHttp.status === 200) {
             // If there's a callback, call it and pass in the response. If not, return the response.
             return callback(JSON.parse(xmlHttp.responseText));
-        } else {
-          return console.error(xmlHttp.statusText);
-        }
-        console.log(xmlHttp.responseText);
-      }
-    };
-    xmlHttp.onerror = function (e) {
-      return console.error(xmlHttp.statusText);
-    };
+	        } else {
+	        	return console.error(xmlHttp.statusText);
+	        }
+	        console.log(xmlHttp.responseText);
+	    }
+	};
+	xmlHttp.onerror = function (e) {
+		return console.error(xmlHttp.statusText);
+	};
 
-    xmlHttp.send();
+	xmlHttp.send();
 };
 
-// TODO: Build results table using Handlebars?
-// Run the query and pass in a callback, to be used when data is returned.
+
+// Generates a bootstrap table in the template, after receiving data.
 AlchemyNewsNetwork.prototype.generateTable = function(data, extraData) {
 	// If I have time, build something cool with the extra data. Wordcloud?
 	var tableContainer = document.getElementById('analysis-table');
@@ -144,26 +158,32 @@ AlchemyNewsNetwork.prototype.generateTable = function(data, extraData) {
 	if (data['keywords']) {
 		data['keywords'].forEach(function(keyword){
 			var row = ['<tr><td>', keyword['text'],'</td><td>', keyword['relevance'],'</td></tr>'].join('');
-			console.log(row);
 			tableBody.insertAdjacentHTML('beforeend', row);
 		}.bind(this));
 		tableContainer.classList.remove('hidden');
 	} else {
-		this.noResults();
+		apiExceeded();
 	}
 };
 
-// TODO: Build results table using Handlebars?
-// Run the query and pass in a callback, to be used when data is returned.
+
+// Pretty prints JSON to display in the template.
 AlchemyNewsNetwork.prototype.prettyPrintJson = function(data) {
 	var elementForJson = document.getElementById('show-json');
 	elementForJson.innerHTML = JSON.stringify(data, null, 2); 
 	elementForJson.classList.remove('hidden');
 };
 
-// If there are no results, show a 'no results' message.
-AlchemyNewsNetwork.prototype.noResults = function() {
+
+// If there are no results, show a 'no results' message in the template.
+var noResults = function() {
 	document.getElementById('no-results').classList.remove('hidden');
+};
+
+
+// If api query limits are exceeded, show a message.
+var apiExceeded = function() {
+	document.getElementById('api-limit').classList.remove('hidden');
 };
 
 // Generates an epoch time span from the user's selected time span to today.
